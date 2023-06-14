edb8752bedc52cebec0c0b815b586ccaebbaed91
CAROLKOECH <koechc44@gmail.com>
2023-06-14 14:43:18 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2; // multiply num1 and num2
+
+const random = Math.floor(Math.random() * 100) + 1; // generate a random integer greater than 0
+
+const num3 = 46;
+const num4 = 6;
+const mod = num3 % num4; // calculate the remainder of num3 divided by num4
+
+const numbers = [5, 12, 20, 3, 8];
+const max = Math.max(...numbers); // find the highest number in the array
+
+console.log(multiply); // Output: 62
+console.log(random); // Output: random integer greater than 0
+console.log(mod); // Output: 4
+console.log(max); // Output: 20
+
+
+  
